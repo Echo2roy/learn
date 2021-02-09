077627105802e6fdb586034314fa15981f1e966b
Roy Esibe <bichiroy50@gmail.com>
2021-02-09 09:42:16 +0100

    learn | echo | Add gradle log4j and slf4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
   java
   application
   id("com.github.johnrengelman.shadow") version "6.1.0"
+  id ("io.spring.dependency-management") version "1.0.1.RELEASE"
+
 }
 
 group = "com.trial.udemy"
@@ -27,8 +29,18 @@ application {
   mainClassName = launcherClassName
 }
 
+dependencyManagement {
+  imports {
+    mavenBom("org.apache.logging.log4j:log4j-bom:2.14.0")
+  }
+}
+
 dependencies {
   implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("org.apache.logging.log4j:log4j-api")
+  implementation("org.apache.logging.log4j:log4j-core")
+  implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+  implementation("org.slf4j:slf4j-api:1.7.30")
   implementation("io.vertx:vertx-core")
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
